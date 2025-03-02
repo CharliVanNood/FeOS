a743cf3e13307f17ac88edf6e2c8d482a6e92fb4
charli <charli.vannood@gmail.com>
2025-03-02 23:24:01 +0100

    tokenizer

--- src/python.rs
+++ src/python.rs
@@ -1,12 +1,17 @@
-use crate::println;
+use crate::{print, println};
 
 pub fn exec(input: [usize; 255]) {
-    let _tokenized_code = tokenize(input);
+    let tokenized_code = tokenize(input);
+
+    for token in tokenized_code {
+        print!("{} ", token);
+    }
+    print!("\n");
 }
 
 fn match_token(token: [i8; 64]) -> i8 {
-    let tokens_val = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "print", "+", "-", "/", "*"];
-    let tokens_keys = [ 0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,       11,  12,  13,  14];
+    let tokens_val = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "print", "+", "-", "/", "*", "(", ")"];
+    let tokens_keys = [ 0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,       11,  12,  13,  14,  15,  16];
 
     for command_index in 0..tokens_val.len() {
         let command = tokens_val[command_index];
@@ -29,7 +34,8 @@ fn match_token(token: [i8; 64]) -> i8 {
 }
 
 fn tokenize(input: [usize; 255]) -> [i8; 255] {
-    let tokens: [i8; 255] = [0; 255];
+    let mut tokens: [i8; 255] = [0; 255];
+    let mut tokens_index = 0;
 
     println!("tokenizing code");
 
@@ -41,16 +47,16 @@ fn tokenize(input: [usize; 255]) -> [i8; 255] {
         let char = input[char_index];
         if char == 0 { continue; }
         if char == 32 {
-            match_token(temp_token);
+            tokens[tokens_index] = match_token(temp_token);
+            tokens_index += 1;
             temp_token = [0; 64];
             temp_token_index = 0;
         } else {
             temp_token[temp_token_index] = char as i8;
             temp_token_index += 1;
         }
-
-        println!("{}", char);
     }
+    tokens[tokens_index] = match_token(temp_token);
 
     tokens
 }
