fd7c2df5b122317108e153d041c837ed446d079b
charli <123391187+CharliVanNood@users.noreply.github.com>
2025-03-03 10:55:50 +0100

    implemented floating point numbers in writing

--- src/python.rs
+++ src/python.rs
@@ -22,7 +22,8 @@ fn shift_list(list: [(i8, i32); 255], index: usize, length: usize) -> [(i8, i32)
 fn run_tokens(tokens: [(i8, i32); 255]) {
     let tokens_after_fact = run_tokens_fact(tokens);
     let tokens_after_math = run_tokens_math(tokens_after_fact);
-    let _tokens_after_top = run_tokens_top(tokens_after_math);
+    let tokens_after_bool = run_tokens_boolean(tokens_after_math);
+    let _tokens_after_top = run_tokens_top(tokens_after_bool);
 }
 
 fn run_tokens_fact(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
@@ -129,6 +130,62 @@ fn run_tokens_math(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
     tokens
 }
 
+fn run_tokens_boolean(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
+    let mut token_index = 0;
+    for _ in 0..255 {
+        let token = tokens[token_index];
+
+        match token.0 {
+            17 => {
+                match (tokens[token_index - 1].0, tokens[token_index + 1].0) {
+                    (1, 1) => {
+                        let mut operation_result = 0;
+                        if tokens[token_index - 1].1 == tokens[token_index + 1].1 {
+                            operation_result = 1;
+                        }
+                        tokens[token_index - 1] = (3, operation_result);
+                        tokens = shift_list(tokens, token_index, 2);
+                        token_index -= 1;
+                    }
+                    (1, 2) => {
+                        let mut operation_result = 0;
+                        if tokens[token_index - 1].1 * 100 == tokens[token_index + 1].1 {
+                            operation_result = 1;
+                        }
+                        tokens[token_index - 1] = (3, operation_result);
+                        tokens = shift_list(tokens, token_index, 2);
+                        token_index -= 1;
+                    }
+                    (2, 1) => {
+                        let mut operation_result = 0;
+                        if tokens[token_index - 1].1 == tokens[token_index + 1].1 * 100 {
+                            operation_result = 1;
+                        }
+                        tokens[token_index - 1] = (3, operation_result);
+                        tokens = shift_list(tokens, token_index, 2);
+                        token_index -= 1;
+                    }
+                    (2, 2) => {
+                        let mut operation_result = 0;
+                        if tokens[token_index - 1].1 == tokens[token_index + 1].1 {
+                            operation_result = 1;
+                        }
+                        tokens[token_index - 1] = (3, operation_result);
+                        tokens = shift_list(tokens, token_index, 2);
+                        token_index -= 1;
+                    }
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+
+        token_index += 1;
+    }
+
+    tokens
+}
+
 fn run_tokens_top(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
     let mut token_index = 0;
     for _ in 0..255 {
@@ -136,12 +193,24 @@ fn run_tokens_top(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
 
         match token.0 {
             10 => {
-                if tokens[token_index + 1].0 == 1 {
-                    println!("{}", tokens[token_index + 1].1);
-                    tokens = shift_list(tokens, token_index, 2);
-                } else if tokens[token_index + 1].0 == 2 {
-                    println!("{}", tokens[token_index + 1].1 as f32 / 100.0);
-                    tokens = shift_list(tokens, token_index, 2);
+                match tokens[token_index + 1].0 {
+                    1 => {
+                        println!("{}", tokens[token_index + 1].1);
+                        tokens = shift_list(tokens, token_index, 2);
+                    }
+                    2 => {
+                        println!("{}", tokens[token_index + 1].1 as f32 / 100.0);
+                        tokens = shift_list(tokens, token_index, 2);
+                    }
+                    3 => {
+                        if tokens[token_index + 1].1 == 0 {
+                            println!("false");
+                        } else {
+                            println!("true");
+                        }
+                        tokens = shift_list(tokens, token_index, 2);
+                    }
+                    _ => println!("This is an unsupported type conversion")
                 }
             },
             _ => {}
@@ -154,8 +223,8 @@ fn run_tokens_top(mut tokens: [(i8, i32); 255]) -> [(i8, i32); 255] {
 }
 
 fn match_token(token: [i8; 64]) -> (i8, i32) {
-    let tokens_val = ["print", "+", "-", "/", "*", "(", ")"];
-    let tokens_keys  = [ 10,      11,  12,  13,  14,  15,  16];
+    let tokens_val = ["print", "+", "-", "/", "*", "(", ")", "=="];
+    let tokens_keys  = [ 10,      11,  12,  13,  14,  15,  16,  17];
 
     for command_index in 0..tokens_val.len() {
         let command = tokens_val[command_index];
@@ -175,12 +244,17 @@ fn match_token(token: [i8; 64]) -> (i8, i32) {
     }
 
     let mut is_int = true;
+    let mut is_float = false;
     let mut int_len = 0;
     for byte in token {
         if byte == 0 { break; }
-        if byte < 48 || byte > 57 {
+        if (byte < 48 || byte > 57) && byte != 46 {
             is_int = false;
             break;
+        }
+        if byte == 46 {
+            is_int = false;
+            is_float = true;
         }
         int_len += 1;
     }
@@ -193,6 +267,30 @@ fn match_token(token: [i8; 64]) -> (i8, i32) {
         }
 
         return (1, int_val)
+    } else if is_float {
+        let mut int_val = 0;
+        let mut dec_place = 0;
+
+        for i in 0..int_len {
+            if token[i] == 46 {
+                dec_place = i;
+                break;
+            }
+        }
+
+        let decimals = (int_len - dec_place) as u32 - 1;
+
+        for i in 0..int_len {
+            if token[i] == 46 {
+                continue;
+            }
+            if i > 1 && token[i - 3] == 46 { break; }
+            let byte_number = token[i] as i32 - 48;
+            if i < dec_place { int_val += byte_number * 10_i32.pow((int_len - i) as u32 - decimals); }
+            else { int_val += byte_number * 10_i32.pow((int_len - i) as u32 - (decimals - 1)); }
+        }
+
+        return (2, int_val)
     }
 
     (-1, 0)
