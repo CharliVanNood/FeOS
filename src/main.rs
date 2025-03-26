9aaf27ababbacbc88a7dceab9680e5fc11a1421c
Charli <charli.vannood@gmail.com>
2025-03-26 23:11:12 +0100

    got disk driver working

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,60 @@ pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
         warnln!("[AWW] Ram test failed :c");
     }
 
+    disk::print_ring();
+
+    let mut read_buffer = [0u16; 256];
+    let write_buffer = [0xABCDu16; 256];
+    disk::write_sector(1, &write_buffer);
+    disk::read_sector(1, &mut read_buffer);
+    let write_successfull = read_buffer == [0xABCDu16; 256];
+    if write_successfull {
+        infoln!("[YAY] Disk write sector 1 was successfull :D");
+    } else {
+        warnln!("[AWW] Disk write sector 1 failed :c");
+        for byte in read_buffer.iter().enumerate() {
+            if byte.0 > 15 {
+                print!("\n");
+                break;
+            }
+            warnln!("{:04x} ", byte.1);
+        }
+    }
+
+    let write_buffer = [0x1234u16; 256];
+    disk::write_sector(2, &write_buffer);
+    disk::read_sector(2, &mut read_buffer);
+    let write_successfull = read_buffer == [0x1234u16; 256];
+    if write_successfull {
+        infoln!("[YAY] Disk write sector 2 was successfull :D");
+    } else {
+        warnln!("[AWW] Disk write sector 2 failed :c");
+        for byte in read_buffer.iter().enumerate() {
+            if byte.0 > 15 {
+                print!("\n");
+                break;
+            }
+            warnln!("{:04x} ", byte.1);
+        }
+    }
+
+    let write_buffer = [0x5678u16; 256];
+    disk::write_sector(3, &write_buffer);
+    disk::read_sector(3, &mut read_buffer);
+    let write_successfull = read_buffer == [0x5678u16; 256];
+    if write_successfull {
+        infoln!("[YAY] Disk write sector 3 was successfull :D");
+    } else {
+        warnln!("[AWW] Disk write sector 3 failed :c");
+        for byte in read_buffer.iter().enumerate() {
+            if byte.0 > 15 {
+                print!("\n");
+                break;
+            }
+            warnln!("{:04x} ", byte.1);
+        }
+    }
+
     println!("Done testing!");
 
     println!("--------------------------------------");
