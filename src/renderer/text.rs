5d936df28627343c69a79e529c3ea7900754ee42
Charli <charli.vannood@gmail.com>
2025-04-05 19:36:39 +0200

    created the first characters

--- src/renderer/text.rs
+++ src/renderer/text.rs
@@ -1,5 +1,61 @@
 static CHARACTER_SIZE: usize = 5 * 7;
 
-static CHARACTERS: [[bool; CHARACTER_SIZE]] = [
-    [false; CHARACTER_SIZE]
+pub static CHARACTERS: [[bool; CHARACTER_SIZE]; 100] = [
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE], [false; CHARACTER_SIZE],
+    [false; CHARACTER_SIZE],
+    
+    [ // character A
+        false, false, false, false, false,
+        false, false, true,  false, false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+        false, true,  true,  true,  false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+    ], 
+    [ // character B
+        false, false, false, false, false,
+        false, true,  true,  false, false,
+        false, true,  false, true,  false,
+        false, true,  true,  false, false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+        false, true,  true,  true,  false,
+    ],
+    [ // character C
+        false, false, false, false, false,
+        false, false, true,  true,  false,
+        false, true,  false, false, false,
+        false, true,  false, false, false,
+        false, true,  false, false, false,
+        false, true,  false, false, false,
+        false, false, true,  true,  false,
+    ],
+    [ // character D
+        false, false, false, false, false,
+        false, true,  true,  false, false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+        false, true,  false, true,  false,
+        false, true,  true,  true,  false,
+    ], 
 ];
